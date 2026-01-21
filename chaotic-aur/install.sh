91496b37c64c46e0caf9cec15a664506342643d3
talned <tal7658@outlook.com>
2026-01-22 05:41:11 +1100

    Added the files

---
+++ chaotic-aur/install.sh
@@ -0,0 +1,14 @@
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+sudo tee -a /etc/pacman.conf << 'EOF'
+
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist
+EOF
+
+sudo pacman -Syyu
+
